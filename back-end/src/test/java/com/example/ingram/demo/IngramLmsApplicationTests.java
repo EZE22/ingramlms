d8bfd1f8c3fc62cb17f09057ce998a12cb5ea427
adam <everett.ingram22@gmail.com>
2021-03-01 19:01:54 -0500

    project creation

---
+++ back-end/src/test/java/com/example/ingram/demo/IngramLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingram.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngramLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
