03c518dab51674701ab715990789d94576c36cf5
adam <everett.ingram22@gmail.com>
2021-03-01 19:20:45 -0500

    fix

--- mysql/project_schema.sql
+++ mysql/project_schema.sql
@@ -14,4 +14,4 @@ CREATE TABLE `student` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 INSERT INTO `student` VALUES 
-(1,'Software Engineer 1', 'Adam', 'Ingram', 'everett.ingram22@gmail.com')
+(1,'Software Engineer', 'Adam', 'Ingram', 'everett.ingram22@gmail.com')
