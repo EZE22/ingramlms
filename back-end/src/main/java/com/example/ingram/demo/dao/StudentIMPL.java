6f6543d4cb56b75d2505d7eba98dcb23cf483dde
adam <everett.ingram22@gmail.com>
2021-03-01 20:46:11 -0500

    Updated Student IMPL

--- back-end/src/main/java/com/example/ingram/demo/dao/StudentIMPL.java
+++ back-end/src/main/java/com/example/ingram/demo/dao/StudentIMPL.java
@@ -1,24 +1,35 @@
 package com.example.ingram.demo.dao;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
+@Repository
 public class StudentIMPL implements StudentDAO{
+    //TODO add the entity manager / Constructor Injection 
+
+
     @Override
+    @Transactional
     public List<Object> findAll() {
         return null;
     }
 
     @Override
+    @Transactional
     public Object findById(int theId) {
         return null;
     }
 
     @Override
+    @Transactional
     public void save(Object theEmployee) {
 
     }
 
     @Override
+    @Transactional
     public void deleteById(int theId) {
 
     }
