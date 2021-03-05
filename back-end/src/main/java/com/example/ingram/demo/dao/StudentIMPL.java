944c8846d334b37b1a03869bdb20dc501bbaee85
adam <everett.ingram22@gmail.com>
2021-03-05 15:02:40 -0500

    Unversioned Files

---
+++ back-end/src/main/java/com/example/ingram/demo/dao/StudentIMPL.java
@@ -0,0 +1,65 @@
+package com.example.ingram.demo.dao;
+
+import com.example.ingram.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class StudentIMPL implements myDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public StudentIMPL(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Object> findAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Object> studentData = currentSession.createQuery("from Student");
+
+        return studentData.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Object saveOrUpdate(Object theStudent) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(theStudent);
+        return theStudent;
+    }
+
+    @Override
+    @Transactional
+    public Object findById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        return currentSession.get(Student.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public String deleteById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Student tempStudent = currentSession.get(Student.class, theId);
+        String failureMessage = "Student not found with id: "
+                + theId
+                + " \nPlease try again. :)";
+        try {
+            if(tempStudent == null) {
+                throw new Exception(failureMessage);
+            }
+            currentSession.delete(tempStudent);
+            return "You have successfully deleted the student with ID: " + theId;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return failureMessage;
+        }
+    }
+}
